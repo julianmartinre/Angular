175cc624107d4aa997f28ef39f08935b450ac2f8
navegador <navegador@090_01_03>
2021-03-11 09:49:23 -0300

    Agregar archivos de proyecto.

---
+++ Angular/Scripts/controller.js
@@ -0,0 +1,14 @@
+﻿angular.module("HttpModule", [])
+.controller("HttpController", function ($scope, $http) {
+    $scope.nombre = "Julian";
+    $scope.posts = [];
+    $http({
+        method: 'GET',
+        url: 'https://jsonplaceholder.typicode.com/posts'
+    }).then(function (response) {
+        console.log(response);
+        $scope.posts = response.data;
+    }, function (error) {
+
+    })
+});
